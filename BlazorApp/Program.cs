016cdd9707bc6e355a76d07909cec766930454fe
valen <valen@DESKTOP-GQP6OPG>
2025-12-05 17:18:18 -0300

    Blazor Inicio

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,43 +1,27 @@
-using Application.Services.Implementations;
-using Application.Services.Interfaces;
 using BlazorApp.Components;
-using Data;
-using Data.Repositories;
-using Data.Repositories.Implementations;
-using Microsoft.EntityFrameworkCore;
-
-
 
 var builder = WebApplication.CreateBuilder(args);
-
-
-
-// Configurar HttpClient para consumir la Web API
-builder.Services.AddHttpClient("WebAPI", client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7001/ ");
-});
-
-
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Configurar HttpClient
+builder.Services.AddHttpClient();
+
+// REGISTRAR AuthService (ESTA LÍNEA FALTABA)
+builder.Services.AddScoped<BlazorApp.Services.AuthService>();
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
